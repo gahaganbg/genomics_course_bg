9519e1975b434a5fad62070f6fe6a4ca944e208c
Ben Gahagan <ben.gahagan@mass.gov>
2020-08-31 10:18:30 -0400

    Completed step 5 of Lab 2

---
+++ LAB1_BIG.R
@@ -0,0 +1,35 @@
+#Lab from 8/24/2020, Excercise completed by Ben Gahagan.
+
+####Exercise 1####
+#Assign Values
+x <- 3
+y <- 15
+#Sum
+x+y
+#Difference
+x-y
+#Product
+x*y
+#Quotient
+x/y
+
+####Exercise 2####
+#Assign Values
+x <- 2
+y <- 3
+#Logical Test
+x^5>y^4
+
+####Exercise 3####
+#Create vector
+vals <- c(22,62,148,43,129)
+#Calculate sum
+sum(vals)
+#Divide each value by sum
+vals/sum(vals)
+
+####Exercise 4####
+#Create vector
+nucs <- c('A','T','C','G')
+#Sort character vector
+sort(nucs)
